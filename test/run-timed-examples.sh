d168b82188487350b76bd096ad18a3121f5406c9
lmshk <lmshk@users.noreply.github.com>
2026-07-30 19:05:42 +0200

    Change timing benchmark specifications
    Take less but longer trajectories. Also reset the timing script root
    seed to make the generated models equivalent to the "kroneckers"
    benchmarks by default.

--- test/run-timed-examples.sh
+++ test/run-timed-examples.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
-set -e
+set -ex
 
+: ${JULIA:=julia}
+: ${SEED:=seed}
 REPOSITORY=$(CDPATH=; cd -- "$(dirname -- "$0")/.." && pwd -P)
 RESULTS="results/timing"
 
-set -x
 mkdir -p "$RESULTS/traces"
 mkdir -p "$RESULTS/statistics"
-: ${JULIA:=julia}
-: ${SEED:=benchmark-1}
-
 for SCHEDULE; do
     NAME="$(basename "${SCHEDULE%.schedule.json}")"
     /usr/bin/time -v --output="$RESULTS/statistics/$NAME-$SEED.txt" \
